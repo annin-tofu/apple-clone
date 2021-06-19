26c3de7b75ada1dad497cc4eedc011738d73ccf7
annin-tofu <yuok5770@gmail.com>
2021-06-20 05:55:30 +0900

    favicon changed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "11bb093148dec287abde"; }
+/******/ 		__webpack_require__.h = function() { return "612366e17f3fa86f2794"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
